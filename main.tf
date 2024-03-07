3b146574ad1e68bd8fa7d794cee65291efbe8749
9three3four <lanka.kv@icloud.com>
2024-03-07 15:39:21 +0530

    provisioning the first instance on AWS

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
+terraform 
 
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 4.16"
+		}
+	}
+	
+	required_version = ">=1.2.0"
+}
+
+provider "aws"	{
+	region = "us-west-2"
+	profile = "sandbox"
+}	
+
+resource "aws_instance" "new_server" {
+	ami = ""
+	instance_type = "t2.micro"
+
+	tags = {
+		Name = "Web Server"
+	}
+}	
