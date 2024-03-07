4c3c929c3c5b42c9fcea2b0480f30a47b1912ae6
9three3four <lanka.kv@icloud.com>
2024-03-07 22:48:42 +0530

    addition 1

--- src/modules/compute/main.tf
+++ src/modules/compute/main.tf
@@ -1,5 +1,16 @@
+// Create aws_ami filter to pick up the ami available in your region
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
 resource "aws_instance" ec2_public{
-	ami = 
+	ami = data.aws_ami.amazon-linux-2.id
 	associate_public_ip = true
 	instance_type = "t2.micro"
 	key_name = var.key_name
