6d721141192e61be5e1c487149421c2a16f92511
9three3four <lanka.kv@icloud.com>
2024-03-07 17:32:30 +0530

    changes1

--- src/main.tf
+++ src/main.tf
@@ -3,17 +3,21 @@ terraform
 	required_providers {
 		aws = {
 			source = "hashicorp/aws"
-			version = "~> 4.16"
+			version = "~> 5.0"
 		}
 	}
 	
 	required_version = ">=1.2.0"
 }
 
+# Configure the AWS provider
+
 provider "aws"	{
 	region = "us-west-2"
 	profile = "sandbox"
 }	
+ 
+# Create an EC2 instance
 
 resource "aws_instance" "new_server" {
 	ami = "ami-08f7912c15ca96832"
